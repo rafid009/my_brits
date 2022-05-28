6fa0b8a7fa1193c675717118a13bb8fdc20be950
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2022-05-28 16:07:59 -0700

    only LTE needed

--- evaluate_imputations.py
+++ evaluate_imputations.py
@@ -414,27 +414,27 @@ start_time = time.time()
 given_features = features
 
 given_features = [
-    'MEAN_AT', # mean temperature is the calculation of (max_f+min_f)/2 and then converted to Celsius. # they use this one
-    'MIN_AT',
-    'AVG_AT', # average temp is AgWeather Network
-    'MAX_AT',
-    'MIN_REL_HUMIDITY',
-    'AVG_REL_HUMIDITY',
-    'MAX_REL_HUMIDITY',
-    'MIN_DEWPT',
-    'AVG_DEWPT',
-    'MAX_DEWPT',
-    'P_INCHES', # precipitation
-    'WS_MPH', # wind speed. if no sensor then value will be na
-    'MAX_WS_MPH', 
-    'LW_UNITY', # leaf wetness sensor
-    'SR_WM2', # solar radiation # different from zengxian
-    'MIN_ST8', # diff from zengxian
-    'ST8', # soil temperature # diff from zengxian
-    'MAX_ST8', # diff from zengxian
-    #'MSLP_HPA', # barrometric pressure # diff from zengxian
-    'ETO', # evaporation of soil water lost to atmosphere
-    'ETR',
+    # 'MEAN_AT', # mean temperature is the calculation of (max_f+min_f)/2 and then converted to Celsius. # they use this one
+    # 'MIN_AT',
+    # 'AVG_AT', # average temp is AgWeather Network
+    # 'MAX_AT',
+    # 'MIN_REL_HUMIDITY',
+    # 'AVG_REL_HUMIDITY',
+    # 'MAX_REL_HUMIDITY',
+    # 'MIN_DEWPT',
+    # 'AVG_DEWPT',
+    # 'MAX_DEWPT',
+    # 'P_INCHES', # precipitation
+    # 'WS_MPH', # wind speed. if no sensor then value will be na
+    # 'MAX_WS_MPH', 
+    # 'LW_UNITY', # leaf wetness sensor
+    # 'SR_WM2', # solar radiation # different from zengxian
+    # 'MIN_ST8', # diff from zengxian
+    # 'ST8', # soil temperature # diff from zengxian
+    # 'MAX_ST8', # diff from zengxian
+    # #'MSLP_HPA', # barrometric pressure # diff from zengxian
+    # 'ETO', # evaporation of soil water lost to atmosphere
+    # 'ETR',
     'LTE50' # ???
 ]
 
