87c41fd691a52f6af5fb18c707c829b5f374263b
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2022-08-20 15:20:55 +0600

    rnn 21-5

--- lam_model/utils.py
+++ lam_model/utils.py
@@ -50,7 +50,7 @@ def split_and_normalize(_df, season_max_length, seasons, features, x_mean, x_std
         y_next = np.concatenate((y_next, add_array_next), axis=0)
         # _y = np.squeeze(_y)
         print(f"y_1: {_y.shape}, y_2: {y_next.shape}")
-        _y = np.concatenate((_y, y_next), axis=0)
+        _y = np.concatenate((_y, y_next), axis=1)
         print(f"y_both: {_y.shape}")
         x.append(_x)
         y.append(_y)
