4e3e3a43112b16d7d87a184d8b2cc02659ff2bd6
Mohammad Rafid Ul Islam <islammoh@dgxs-1.hpc.engr.oregonstate.edu>
2022-08-21 01:11:07 -0700

    21-8

--- main.py
+++ main.py
@@ -231,7 +231,7 @@ if __name__ == "__main__":
     X_intact, X, missing_mask, indicating_mask = mcar(X, 0.1) # hold out 10% observed values as ground truth
     X = masked_fill(X, 1 - missing_mask, np.nan)
     # Model training. This is PyPOTS showtime. 
-    saits = SAITS(n_steps=252, n_features=len(features), n_layers=2, d_model=256, d_inner=128, n_head=8, d_k=64, d_v=64, dropout=0.1, epochs=2000, patience=100)
+    saits = SAITS(n_steps=252, n_features=len(features), n_layers=2, d_model=256, d_inner=128, n_head=4, d_k=64, d_v=64, dropout=0.1, epochs=2500, patience=100)
 
     saits.fit(X)  # train the model. Here I use the whole dataset as the training set, because ground truth is not visible to the model.
     pickle.dump(saits, open(filename, 'wb'))
