dd2c73b205a431a2afb2e2b964df2f51f073eb6b
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2022-08-22 08:36:25 +0600

    mice check 1

--- evaluate_imputations.py
+++ evaluate_imputations.py
@@ -635,7 +635,7 @@ def evaluate_imputation(mse_folder):
                     imputation_brits = ret['imputations'].data.cpu().numpy()
                     imputation_brits = np.squeeze(imputation_brits)
                     imputed_brits = imputation_brits[row_indices, feature_idx]#unnormalize(imputation_brits[row_indices, feature_idx], mean, std, feature_idx)
-                    
+                    prinbt(f"imputed brits: {imputed_brits}")
                     Xeval = np.reshape(Xeval, (1, Xeval.shape[0], Xeval.shape[1]))
                     # X_intact, Xe, missing_mask, indicating_mask = mcar(Xeval, 0.1) # hold out 10% observed values as ground truth
                     
@@ -644,6 +644,7 @@ def evaluate_imputation(mse_folder):
                     # print(f"SAITS imputation: {imputation_saits}")
                     imputation_saits = np.squeeze(imputation_saits)
                     imputed_saits = imputation_saits[row_indices, feature_idx]
+                    print(f"imputed saits: {imputed_saits}")
 
                     ret_eval = copy.deepcopy(eval_)
                     ret_eval[row_indices, feature_idx] = np.nan
@@ -723,16 +724,18 @@ def evaluate_imputation(mse_folder):
                     }
 
                     transformer_preds = run_transformer(params)
-                    # # print(f'trasformer preds: {transformer_preds.shape}')
+                    # print(f'trasformer preds: {transformer_preds}')
                     
                     imputation_transformer = np.squeeze(transformer_preds)
                     imputed_transformer = imputation_transformer[row_indices, feature_idx].cpu().detach().numpy()
+                    print(f"imputed mvts: {imputed_transformer}")
 
                     ret_eval[row_indices, feature_idx] = np.nan
                     ret_eval_df = pd.DataFrame(ret_eval, columns=features)
                     imputed_linear = ret_eval_df.interpolate(method='linear', limit_direction='both')
                     imputed_linear = imputed_linear.to_numpy()
                     imputed_linear = imputed_linear[row_indices, feature_idx]
+                    print(f"imputd linear: {imputed_linear}")
 
                     real_values = eval_[row_indices, feature_idx]
                     print(f"real: {real_values}")
